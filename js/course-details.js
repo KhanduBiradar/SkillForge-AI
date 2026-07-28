d3f45f2d1caea003c74a8f8d42141084682c43f5
Khandu Biradar <khandubiradar2426@gmail.com>
2026-07-28 17:23:57 +0530

    Added video learning section to course details

--- js/course-details.js
+++ js/course-details.js
@@ -3,3 +3,8 @@ document.querySelector(".start-btn").addEventListener("click", function () {
     alert("Course Started Successfully 🚀");
 
 });
+document.querySelector(".start-btn").addEventListener("click", function () {
+
+    alert("🎉 Congratulations! Course Completed Successfully.");
+
+});

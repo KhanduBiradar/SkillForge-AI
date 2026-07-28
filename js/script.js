4e6fd81238c091208f46424968fba37826d6e9f3
Khandu Biradar <khandubiradar2426@gmail.com>
2026-07-28 16:28:28 +0530

    Added signup form validation and backend integration

--- js/script.js
+++ js/script.js
@@ -94,3 +94,42 @@ if (form) {
 }
 // Dashboard loaded successfully
 console.log("SkillForge AI Dashboard Loaded");
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    try {
+        const fullname = document.getElementById("fullname").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        const response = await fetch("http://127.0.0.1:5000/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                fullname,
+                email,
+                password
+            })
+        });
+
+        const result = await response.json();
+
+        alert(result.message);
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+        console.error(error);
+        alert("Error: " + error.message);
+    }
+});

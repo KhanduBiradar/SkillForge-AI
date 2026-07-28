5d4b6768240736b23a7c2dd30bfefc2f97ebc737
Khandu Biradar <khandubiradar2426@gmail.com>
2026-07-28 17:27:02 +0530

    Added automatic course progress tracker

--- js/course-details.js
+++ js/course-details.js
@@ -8,3 +8,42 @@ document.querySelector(".start-btn").addEventListener("click", function () {
     alert("🎉 Congratulations! Course Completed Successfully.");
 
 });
+const lessons = document.querySelectorAll(".lesson input");
+
+const progress = document.getElementById("progress");
+
+const progressText = document.getElementById("progressText");
+
+lessons.forEach(box => {
+
+    box.addEventListener("change", updateProgress);
+
+});
+
+function updateProgress(){
+
+    let completed = 0;
+
+    lessons.forEach(box=>{
+
+        if(box.checked){
+
+            completed++;
+
+        }
+
+    });
+
+    const percent = Math.round((completed / lessons.length) * 100);
+
+    progress.style.width = percent + "%";
+
+    progressText.innerHTML = percent + "% Completed";
+
+    if(percent===100){
+
+        alert("🏆 Congratulations!\nCourse Completed Successfully.");
+
+    }
+
+}

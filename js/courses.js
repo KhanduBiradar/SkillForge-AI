fda2914b090f4bad7ee3f2586e659f16a77f06f8
Khandu Biradar <khandubiradar2426@gmail.com>
2026-07-29 19:53:04 +0530

    Added course enrollment frontend

--- js/courses.js
+++ js/courses.js
@@ -1,1 +1,43 @@
-localStorage.setItem("course", course);
+
+async function enrollCourse(course) {
+
+    const email = localStorage.getItem("email");
+
+    if (!email) {
+        alert("Please login first.");
+        return;
+    }
+
+    // Certificate साठी course save
+    localStorage.setItem("course", course);
+
+    try {
+
+        const response = await fetch("http://127.0.0.1:5000/enroll", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                email,
+                course
+            })
+
+        });
+
+        const result = await response.json();
+
+        alert(result.message);
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Enrollment Failed");
+
+    }
+
+}
